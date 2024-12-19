dac9d2d5a4cf59bc4976fb5496a1c1b1aa133fec
Maximiliano Paris <mparis@hotmail.cl>
2024-12-18 20:38:11 -0700

    Initial Commit

---
+++ Improvify/ImprovifyApp.swift
@@ -0,0 +1,32 @@
+//
+//  ImprovifyApp.swift
+//  Improvify
+//
+//  Created by Maximiliano París Gaete on 12/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ImprovifyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Improvify/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Improvify
+//
+//  Created by Maximiliano París Gaete on 12/18/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
