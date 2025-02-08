f09b8b58745c0194e72678088ef18bc99c824733
Maximiliano Paris <mparis@hotmail.cl>
2025-02-07 23:16:26 -0700

    fix: logic to generate weekly data

--- Improvify/viewModel/HabitGraphManager.swift
+++ Improvify/viewModel/HabitGraphManager.swift
@@ -41,16 +41,17 @@ class HabitGraphManager {
 
     //MARK: - Logic
 
-    func lastMonday(using calendar: Calendar = .current) -> Date? {
-        let today = calendar.startOfDay(for: Date()) // Normalize to start of day
+    func lastWeekStartDay(using calendar: Calendar = .current) -> Date? {
+        let firstWeekday = calendar.firstWeekday
+        let today = calendar.startOfDay(for: Date()) // Normalize today
         let weekday = calendar.component(.weekday, from: today)
         
         // If today is monday, return today
-        if weekday == 2 {
+        if weekday == firstWeekday {
             return today
         }
         
-        return calendar.nextDate(after: today, matching: DateComponents(weekday: 2), matchingPolicy: .previousTimePreservingSmallerComponents, direction: .backward)
+        return calendar.nextDate(after: today, matching: DateComponents(weekday: firstWeekday), matchingPolicy: .previousTimePreservingSmallerComponents, direction: .backward)
     }
 
     
@@ -81,36 +82,26 @@ class HabitGraphManager {
     }
     
     func generateWeeklyData() {
-        if let lastMonday = lastMonday(using: calendar) {
+        if let lastWeekStartDay = lastWeekStartDay(using: calendar) {
             for i in 0..<15 {
                 
-                // Monday = first day of week
-                // Sunday = last day of week
-                if let monday = calendar.date(byAdding: .day, value: 7*(i-14), to: lastMonday),
-                    let sunday = calendar.date(byAdding: .day, value: 6, to: monday) {
-                    last15recurrences.append(monday)
-                    
-                    let normalizedMonday = calendar.startOfDay(for: monday)
-                    let normalizedSunday = calendar.startOfDay(for: sunday)
+                if let recurrentWeekStartDay = calendar.date(byAdding: .weekOfYear, value: i-14, to: lastWeekStartDay){
+                    last15recurrences.append(recurrentWeekStartDay)
                     
                     let previousWeekCount = cumulativeCounts.last ?? 0
                     
-                    /// This `contains` statement checks if there is at least one date in the `completed` array
-                    /// that is between monday and sunday (inclusive), in that week.
-                    /// If so, that means that habit was completed in that week.
                     if habit.completed.contains(where: { date in
-                        let normalizedDate = calendar.startOfDay(for: date)
-                        return normalizedMonday...normalizedSunday ~= normalizedDate
+                        calendar.isDate(date, equalTo: recurrentWeekStartDay, toGranularity: .weekOfYear)
                     }) {
                         //+1
                         let thisWeekCount = previousWeekCount+1
                         cumulativeCounts.append(thisWeekCount)
-                        data.append(LineChartElement(date: monday, value: thisWeekCount))
+                        data.append(LineChartElement(date: recurrentWeekStartDay, value: thisWeekCount))
                     } else { //NOT completed on that week
                         //-1
                         let thisDayCount = max(previousWeekCount-1, 0) //Never goes under 0
                         cumulativeCounts.append(thisDayCount)
-                        data.append(LineChartElement(date: monday, value: 0))
+                        data.append(LineChartElement(date: recurrentWeekStartDay, value: 0))
                     }
                 }
             }
